576c318da3567c348c641736a8f148fcb3f78c0a
prashikjaware <prashikjaware123@gmail.com>
2021-03-29 17:52:43 +0530

    [Prashik]Added: Delete element with the help of given Key

--- DataStructure/HashTable/MyMapNode.cs
+++ DataStructure/HashTable/MyMapNode.cs
@@ -26,6 +26,26 @@ namespace HashTable
             linkedList.AddLast(item);
         }
 
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
+
         protected int GetArrayPosition(K key)
         {
             int position = key.GetHashCode() % size;
@@ -85,6 +105,19 @@ namespace HashTable
             return frequency;
         }
 
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
 
     }
 
